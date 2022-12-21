57978c13ee105bfc737b6369483f3945df055a7e
Maxim Castornyi <nets.devs@gmail.com>
2022-12-21 11:05:25 +0200

    React Food Delivery App

---
+++ src/assets/fake-data/products.js
@@ -0,0 +1,183 @@
+// all images imported from images directory
+import product_01_image_01 from "../images/product_01.jpg";
+import product_01_image_02 from "../images/product_01.1.jpg";
+import product_01_image_03 from "../images/product_01.3.jpg";
+
+import product_02_image_01 from "../images/product_2.1.jpg";
+import product_02_image_02 from "../images/product_2.2.jpg";
+import product_02_image_03 from "../images/product_2.3.jpg";
+
+import product_03_image_01 from "../images/product_3.1.jpg";
+import product_03_image_02 from "../images/product_3.2.jpg";
+import product_03_image_03 from "../images/product_3.3.jpg";
+
+import product_04_image_01 from "../images/product_4.1.jpg";
+import product_04_image_02 from "../images/product_4.2.jpg";
+import product_04_image_03 from "../images/product_4.3.png";
+
+import product_05_image_01 from "../images/product_04.jpg";
+import product_05_image_02 from "../images/product_08.jpg";
+import product_05_image_03 from "../images/product_09.jpg";
+
+import product_06_image_01 from "../images/bread(1).png";
+import product_06_image_02 from "../images/bread(2).png";
+import product_06_image_03 from "../images/bread(3).png";
+
+const products = [
+  {
+    id: "01",
+    title: "Chicken Burger",
+    price: 24.0,
+    image01: product_01_image_01,
+    image02: product_01_image_02,
+    image03: product_01_image_03,
+    category: "Burger",
+
+    desc: "Two slices of beef patty in a sesame seed bun and temptingly matched with onions, two slices of pickled cucumber, a slice of Cheddar cheese, fresh salad and Big Mac sauce.",
+  },
+
+  {
+    id: "02",
+    title: "Vegetarian Pizza",
+    price: 115.0,
+    image01: product_02_image_01,
+    image02: product_02_image_02,
+    image03: product_02_image_03,
+    category: "Pizza",
+
+    desc: "Pizza with ripe tomatoes sauce, fresh basil and mozzarella cheese.",
+  },
+
+  {
+    id: "03",
+    title: "Double Cheese Margherita",
+    price: 110.0,
+    image01: product_03_image_01,
+    image02: product_03_image_02,
+    image03: product_03_image_03,
+    category: "Pizza",
+
+    desc: "Pizza with chicken, salami, bacon and with barbecue sauce.",
+  },
+
+  {
+    id: "04",
+    title: "Maxican Green Wave",
+    price: 110.0,
+    image01: product_04_image_01,
+    image02: product_04_image_02,
+    image03: product_04_image_03,
+    category: "Pizza",
+
+    desc: "A slice of beef patty, grilled, seasoned with salt and pepper, mustard and ketchup, onions, a slice of pickled cucumber, carefully placed in a caramelized bun.",
+  },
+
+  {
+    id: "05",
+    title: "Cheese Burger",
+    price: 24.0,
+    image01: product_05_image_01,
+    image02: product_05_image_02,
+    image03: product_05_image_03,
+    category: "Burger",
+
+    desc: "A large slice of beef patty, in a sesame bun and paired with two slices of Cheddar cheese, onion, two slices of cucumber and two slices of tomato, salad and chicken sauce.",
+  },
+  {
+    id: "06",
+    title: "Royal Cheese Burger",
+    price: 24.0,
+    image01: product_01_image_01,
+    image02: product_01_image_02,
+    image03: product_01_image_03,
+    category: "Burger",
+
+    desc: "Two crispy chicken fillets, chicken sauce, lettuce, tomato slice and cucumber slice carefully placed in a caramelized bun.",
+  },
+
+  {
+    id: "07",
+    title: "Seafood Pizza",
+    price: 115.0,
+    image01: product_02_image_02,
+    image02: product_02_image_01,
+    image03: product_02_image_03,
+    category: "Pizza",
+
+    desc: "Pizza with ham, pickled mushrooms, pickled eggplants, sheep outbuilding and mozzarella cheese.",
+  },
+
+  {
+    id: "08",
+    title: "Thin Cheese Pizza",
+    price: 110.0,
+    image01: product_03_image_02,
+    image02: product_03_image_01,
+    image03: product_03_image_03,
+    category: "Pizza",
+
+    desc: "Weathered dough for pizza, red sauce for pizza, ham, mushrooms, cheese, black and green olives.",
+  },
+
+  {
+    id: "09",
+    title: "Pizza With Mushroom",
+    price: 110.0,
+    image01: product_04_image_02,
+    image02: product_04_image_01,
+    image03: product_04_image_03,
+    category: "Pizza",
+
+    desc: "Pizza with chicken meat, champignons, butter, truffle oil and parmesan cheese.",
+  },
+
+  {
+    id: "10",
+    title: "Classic Hamburger",
+    price: 24.0,
+    image01: product_05_image_02,
+    image02: product_05_image_01,
+    image03: product_05_image_03,
+    category: "Burger",
+
+    desc: "A slice of beef patty, grilled, seasoned with salt and pepper, mustard and ketchup, onions, a slice of pickled cucumber, carefully placed in a caramelized bun.",
+  },
+
+  {
+    id: "11",
+    title: "Crunchy Bread ",
+    price: 35.0,
+    image01: product_06_image_01,
+    image02: product_06_image_02,
+    image03: product_06_image_03,
+    category: "Bread",
+
+    desc: "Two slices of Emmentaler cheese, a slice of Toast ham, placed in a hot caramelized bun.",
+  },
+
+  {
+    id: "12",
+    title: "Delicious Bread ",
+    price: 35.0,
+    image01: product_06_image_02,
+    image02: product_06_image_01,
+    image03: product_06_image_03,
+    category: "Bread",
+
+    desc: "Puff pastry croissant with 2 slices of Emmental cheese and 2 slices of ham.",
+  },
+
+  {
+    id: "13",
+    title: "Loaf Bread ",
+    price: 35.0,
+    image01: product_06_image_03,
+    image02: product_06_image_02,
+    image03: product_06_image_03,
+    category: "Bread",
+
+    desc: "A special sandwich with ham and Emmentaler cheese, wrapped in a special lavash.",
+  },
+];
+
+export default products;
---
+++ src/components/UI/slider/TestimonialSlider.jsx
@@ -0,0 +1,63 @@
+import React from "react";
+import Slider from "react-slick";
+
+import ava01 from "../../../assets/images/ava-1.jpg";
+import ava02 from "../../../assets/images/ava-2.jpg";
+import ava03 from "../../../assets/images/ava-3.jpg";
+
+import "../../../styles/slider.css";
+
+const TestimonialSlider = () => {
+  const settings = {
+    dots: true,
+    autoplay: true,
+    infinite: true,
+    speed: 1000,
+    autoplaySpeed: 3000,
+    swipeToSlide: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+  };
+  return (
+    <Slider {...settings}>
+      <div>
+        <p className="review__text">
+          "Brands may from time to time revise the information, services and the
+          resources contained in this web site and reserves the right to make
+          such changes without any obligation to notify past, current or
+          prospective visitors."
+        </p>
+        <div className=" slider__content d-flex align-items-center gap-3 ">
+          <img src={ava01} alt="avatar" className=" rounded" />
+          <h6>Jhon Doevo</h6>
+        </div>
+      </div>
+      <div>
+        <p className="review__text">
+          "Brands and, except for personal use, may not be copied, distributed,
+          or reproduced or transmitted in any form or by any mean, electronic,
+          mechanical, photocopying, recording or otherwise, without the prior
+          written permission."
+        </p>
+        <div className="slider__content d-flex align-items-center gap-3 ">
+          <img src={ava02} alt="avatar" className=" rounded" />
+          <h6>Mitchell Marsh</h6>
+        </div>
+      </div>
+      <div>
+        <p className="review__text">
+          "The Site may also contain references to other company, brand and
+          product names. These company, brand and product names are used herein
+          for identification purposes only and may be the trademarks of their
+          respective owners."
+        </p>
+        <div className="slider__content d-flex align-items-center gap-3 ">
+          <img src={ava03} alt="avatar" className=" rounded" />
+          <h6>Steven Crock</h6>
+        </div>
+      </div>
+    </Slider>
+  );
+};
+
+export default TestimonialSlider;
